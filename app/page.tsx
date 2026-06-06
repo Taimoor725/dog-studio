2c82a50cc99a698eaf031d071cdebd704346fd44
Taimoor725 <taimoor725tanveer@gmail.com>
2026-06-06 13:37:47 -0700

    animations created/gsap

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
 "use client"
-import Image from "next/image";
 import Dog from "../components/Dog"
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 
 export default function Home() {
   return (
-    <div className=" w-screen h-screen bg-black">
-
-      <Canvas className="w-screen h-screen">
-        <OrbitControls/>
-        {/* <ambientLight intensity={10}/> */}
-        <directionalLight intensity={10} position={[0,2,5]}/>
-        <Dog/>
+    <div className="w-full min-h-screen bg-black">
+      <div className="w-full h-screen fixed">
+      <Canvas className="fixed top-0 left-0 w-full h-screen z-0 bg-[url('/background-l.png')] bg-cover bg-no-repeat">
+        <Dog />
       </Canvas>
+      </div>
+
+      <section id="section-1" className="relative z-10 h-screen border border-white border-solid"></section>
+      <section id="section-2" className="relative z-10 h-screen border border-white border-solid"></section>
+      <section id="section-3" className="relative z-10 h-screen border border-white border-solid"></section>
+
     </div>
   );
 }
--- components/Dog.jsx
+++ components/Dog.jsx
@@ -1,20 +1,60 @@
 "use client"
+import { useGSAP } from "@gsap/react"
 import { useAnimations, useGLTF, useTexture } from "@react-three/drei"
 import { useThree } from "@react-three/fiber"
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import * as Three from "three"
-import { texture } from "three/tsl"
+import gsap from "gsap"
+import ScrollTrigger from "gsap/ScrollTrigger"
 
 function Dog() {
 
+    gsap.registerPlugin(useGSAP());
+    gsap.registerPlugin(ScrollTrigger);
 
 
 
+
+  const modalRef = useRef();
   const { scene , animations} = useGLTF("/models/dog.glb")
   const {actions } = useAnimations(animations , scene)
   useEffect(()=>{
     actions["Take 001"].play()
   },[actions])
+
+
+    useGSAP(()=>{
+      const tl = gsap.timeline({
+        scrollTrigger:{
+            trigger:"#section-1",
+            endTrigger:"#section-3",
+            start:"top top",
+            end:"bottom bottom",
+            markers:true,
+            scrub:true,
+        }
+      })
+      tl.
+      to(modalRef.current.position , {
+        z:"-0.45",
+        y:"-0.55"
+      })
+      .to(modalRef.current.rotation , {
+        x:`${Math.PI/17}`
+      })
+      .to(modalRef.current.rotation , {
+        y:`-=${Math.PI}`,
+        x:`+=${Math.PI/13}`,
+        z:`+=${Math.PI/15}`
+      },"thired")
+      .to(modalRef.current.position , {
+        x:"-0.4",
+        z:"-0.1",
+        // y:`-=0.01`
+      },"thired")
+    },[])
+
+
 
   useThree(({camera , scene , gl})=>{
     camera.position.z=0.6;
@@ -22,28 +62,37 @@ function Dog() {
     gl.outputColorSpace = Three.SRGBColorSpace;
   })
 
-
-
-
-  const [normalMap , matcap] = (useTexture(["/dog_normals.jpg" , "/matCap/mat-2.png"])).map(texture=>{
+  const [normalMap , matcap , branchNormalMap , branchMatCap] = (useTexture(["/dog_normals.jpg" , "/matCap/mat-2.png","/branches_normals.jpg","/branches_diffuse.jpg"])).map(texture=>{
       texture.flipY = false;
       texture.colorSpace = Three.SRGBColorSpace;
       return texture
   })
+
+
+
+  
 
 const DogMaterial = new Three.MeshMatcapMaterial({
         normalMap:normalMap,
         matcap:matcap
       })
 
+  const branchMaterial = new Three.MeshMatcapMaterial({
+    normalMap : branchNormalMap,  
+    map : branchMatCap
+  })
+
   scene.traverse((parts) => {
     if (parts.name.includes("DOG")) {
       parts.material = DogMaterial;
     }
+    else{
+      parts.material = branchMaterial;
+    }
   })
 
   return (
-    <mesh position={[0.25, -0.6, 0]} scale={[1.1, 1.1, 1.1]} rotation={[0, Math.PI / 3.8, 0]}>
+    <mesh ref={modalRef} position={[0.25, -0.6, 0]} scale={[1.1, 1.1, 1.1]} rotation={[0, Math.PI / 3.8, 0]}>
       <primitive object={scene} />
     </mesh>
   )
